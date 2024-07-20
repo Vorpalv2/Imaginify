68a69945a5308404723f2d96344f758e0267c302
Aakash <aakashanand1995@gmail.com>
2024-07-20 17:24:40 +0530

    saturday commit

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { useParams } from "next/navigation";
 import React from "react";
 
 const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+  const param = useParams();
+  return <div>AddTransformationTypePage:{param.type}</div>;
 };
 
 export default AddTransformationTypePage;
